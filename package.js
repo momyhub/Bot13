5d456a41c0e94851f633b9e7e38915a44aafa56c
momyhub <165927126+momyhub@users.noreply.github.com>
2024-04-12 17:26:45 +0000

    Create package.js

---
+++ package.js
@@ -0,0 +1,20 @@
+
+{
+  "name": "bot13",
+  "version": "1.0.0",
+  "description": "bot vip buy ",
+  "main": "bot.js",
+  
+  "dependencies": {
+    "node-telegram-bot-api": "^0.59.0",
+    "lodash": "^4.17.21",
+    "node-schedule": "^1.3.2"
+  
+
+  },
+  "scripts": {
+    "start": "node bot.js"
+  },
+  "author": "solkah",
+  "license": "MIT"
+  }
